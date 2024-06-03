be70fb2fd71e66b526d31b04c0132f578df9d7a5
enjoeyland <khmin1104@gmail.com>
2024-06-03 06:32:13 +0000

    chore: Add script for finding hyperparameters

--- scripts/bloom_original_train.sh
+++ scripts/bloom_original_train.sh
@@ -4,10 +4,11 @@ set -eou pipefail
 IFS=$'\n\t'
 
 world_size=1
+batch_size=64
 
 python run.py \
-    --model_name bloomz-560m \
-    --model bigscience/bloomz-560m \
+    --model_name bloom-560M \
+    --model bigscience/bloom-560M \
     --method original \
     --cache_dir ../.cache \
     --task xnli \
@@ -16,19 +17,20 @@ python run.py \
     --data_dir ../../research/multilingual-unlearning/data/ \
     --num_workers 4 \
     --do_train \
-    --seed 42 \
     --use_lora \
+    --load_in_4bit \
+    --seed 42 \
     --dp_strategy auto \
     --bf16 \
     --optimizer adamw \
-    --learning_rate 3e-5 \
+    --learning_rate 3e-4 \
     --lr_scheduler_type linear \
     --warmup_ratio 0.1 \
     --epochs 3 \
     --world_size $world_size \
-    --per_device_batch_size 8 \
-    --gradient_accumulation_steps 16 \
-    --logging_steps 200 \
-    --eval_steps 1000 \
-    --max_tolerance 13 \
+    --per_device_batch_size $batch_size \
+    --gradient_accumulation_steps $((128 / batch_size)) \
+    --logging_steps $((1600 / batch_size)) \
+    --eval_steps $((8000 / batch_size)) \
+    --max_tolerance 15 \
     --output_dir ".checkpoints/"
